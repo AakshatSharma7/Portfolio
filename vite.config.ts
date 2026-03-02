f57c7bbdc42fb6d4415bbbe0a995cb19cdf5f36f
Akshat Sharma <akshatssharma868@gmail.com>
2026-03-03 00:17:30 +0530

    Done

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/akshat-sharma-portfolio/',
+    base: '/Portfolio/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
